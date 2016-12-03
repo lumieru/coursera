aa247b229952633e936189f78a4f06754792ded4
LuBin <lumieru@gmail.com>
2016-12-03 20:48:48 +0800

    week2 homework

---
+++ crypto/week2/AESTest.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"crypto/aes"
+	"log"
+	"crypto/rand"
+	"encoding/hex"
+)
+
+const (
+	TYPE_CBC int = iota
+	TYPE_CTR
+)
+
+type AESData struct {
+	message 	[]byte
+	ciphertext 	[]byte
+	key			[]byte
+	mode 		int
+}
+
+var datas = [...]AESData{
+	{[]byte("Basic CBC mode encryption needs padding."), []byte("4ca00ff4c898d61e1edbf1800618fb2828a226d160dad07883d04e008a7897ee2e4b7465d5290d0c0e6c6822236e1daafb94ffe0c5da05d9476be028ad7c1d81"), []byte("140b41b22a29beb4061bda66b6747e14"), TYPE_CBC},
+	{[]byte("Our implementation uses rand. IV"), []byte("5b68629feb8606f9a6667670b75b38a5b4832d0f26e1ab7da33249de7d4afc48e713ac646ace36e872ad5fb8a512428a6e21364b0c374df45503473c5242a253"), []byte("140b41b22a29beb4061bda66b6747e14"), TYPE_CBC},
+	{[]byte("CTR mode lets you build a stream cipher from a block cipher."), []byte("69dda8455c7dd4254bf353b773304eec0ec7702330098ce7f7520d1cbbb20fc388d1b0adb5054dbd7370849dbf0b88d393f252e764f1f5f7ad97ef79d59ce29f5f51eeca32eabedd9afa9329"), []byte("36f18357be4dbd77f050515c73fcf9f2"), TYPE_CTR},
+	{[]byte("Always avoid the two time pad!"), []byte("770b80259ec33beb2561358a9f2dc617e46218c0a53cbeca695ae45faa8952aa0e311bde9d4e01726d3184c34451"), []byte("36f18357be4dbd77f050515c73fcf9f2"), TYPE_CTR},
+}
+
+func Encrypt() {
+	iv := make([]byte, 16)
+	for i:=0; i<len(datas); i++ {
+		binKey := make([]byte, hex.DecodedLen(len(datas[i].key)))
+		_, err := hex.Decode(binKey, datas[i].key)
+		if err != nil {
+			log.Printf("Decode hex key failed:%s\n", err.Error())
+			return
+		}
+		aesCiper, err := aes.NewCipher(binKey)
+		if err != nil {
+			log.Printf("Create aes cipher failed:%s\n", err.Error())
+			return
+		}
+
+		_, err = rand.Read(iv)
+		if err != nil {
+			log.Printf("Create iv failed:%s\n", err.Error())
+			return
+		}
+
+		var dst []byte
+		if datas[i].mode == TYPE_CBC {
+			enc := NewMyCBCEncrypter(aesCiper, iv)
+			dst = make([]byte, enc.EncryptedSize(len(datas[i].message)))
+			enc.CryptBlocks(&dst,  datas[i].message)
+		} else {
+			enc := NewMyCTR(aesCiper, iv)
+			dst = make([]byte, enc.EncryptedSize(len(datas[i].message)))
+			enc.XORKeyStream(&dst, datas[i].message, true)
+		}
+
+		log.Printf("src: %s => Dst:%x\n", datas[i].message, dst)
+	}
+}
+
+func Decrypt() {
+	iv := make([]byte, 16)
+	for i:=0; i<len(datas); i++ {
+		binKey := make([]byte, hex.DecodedLen(len(datas[i].key)))
+		_, err := hex.Decode(binKey, datas[i].key)
+		if err != nil {
+			log.Printf("Decode hex key failed:%s\n", err.Error())
+			return
+		}
+		aesCiper, err := aes.NewCipher(binKey)
+		if err != nil {
+			log.Printf("Create aes cipher failed:%s\n", err.Error())
+			return
+		}
+
+		_, err = rand.Read(iv)
+		if err != nil {
+			log.Printf("Create iv failed:%s\n", err.Error())
+			return
+		}
+
+		binBuffer := make([]byte, hex.DecodedLen(len(datas[i].ciphertext)))
+		_, err = hex.Decode(binBuffer, datas[i].ciphertext)
+		if err != nil {
+			log.Printf("Decode hex failed:%s\n", err.Error())
+			return
+		}
+		dst := make([]byte, len(binBuffer))
+
+		if datas[i].mode == TYPE_CBC {
+			enc := NewMyCBCDecrypter(aesCiper, iv)
+			enc.CryptBlocks(&dst,  binBuffer)
+		} else {
+			enc := NewMyCTR(aesCiper, iv)
+			enc.XORKeyStream(&dst, binBuffer, false)
+		}
+
+		log.Printf("src: %s => Dst:%s\n", datas[i].message, dst)
+	}
+}
+
+func main() {
+	//Encrypt()
+	Decrypt()
+}
---
+++ crypto/week2/MyChiper.go
@@ -0,0 +1,215 @@
+package main
+
+import (
+	"crypto/cipher"
+	"encoding/binary"
+)
+
+type MyCBCEncrypter struct {
+	iv 		[]byte
+	block 	cipher.Block
+}
+
+// BlockSize returns the mode's block size.
+func (enc *MyCBCEncrypter) BlockSize() int {
+	return enc.block.BlockSize()
+}
+
+func (enc *MyCBCEncrypter) EncryptedSize(srcLen int) int {
+	// source len + iv len + padding len
+	return srcLen + enc.BlockSize() + enc.BlockSize() - srcLen % enc.BlockSize()
+}
+
+// CryptBlocks encrypts or decrypts a number of blocks. The length of
+// src must be a multiple of the block size. Dst and src may point to
+// the same memory.
+func (enc *MyCBCEncrypter) CryptBlocks(dst *[]byte, src []byte) {
+	if len(*dst) < enc.EncryptedSize(len(src)) {
+		panic("len(dst) < enc.EncryptedSize(len(src)")
+	}
+
+	//set iv to dst
+	copy(*dst, enc.iv)
+
+	blockSize := enc.BlockSize()
+	srcLen := len(src)
+
+	i := blockSize
+	j := 0
+	key := enc.iv
+	tempBuff := make([]byte, blockSize)
+
+	for {
+		if srcLen - j >= blockSize {
+			enc.block.Encrypt((*dst)[i:i+blockSize], xorSlice(tempBuff, src[j:j+blockSize], key))
+		} else {
+			//padding
+			padded := make([]byte, blockSize)
+			copy(padded, src[j:])
+			remained := len(src[j:])
+			paddingSize := blockSize - remained
+			for k:=0; k<paddingSize; k++ {
+				padded[remained+k] = (byte)(paddingSize)
+			}
+
+			enc.block.Encrypt((*dst)[i:i+blockSize], xorSlice(tempBuff, padded, key))
+			*dst = (*dst)[:i+blockSize]
+			break
+		}
+
+		key = (*dst)[i:i+blockSize]
+		i += blockSize
+		j += blockSize
+	}
+}
+
+func NewMyCBCEncrypter(b cipher.Block, iv []byte) *MyCBCEncrypter {
+	if len(iv) != b.BlockSize() {
+		return nil
+	}
+
+	return &MyCBCEncrypter {
+		iv: iv,
+		block: b,
+	}
+}
+
+type MyCBCDecrypter struct {
+	iv 		[]byte
+	block 	cipher.Block
+}
+
+// BlockSize returns the mode's block size.
+func (dec *MyCBCDecrypter) BlockSize() int {
+	return dec.block.BlockSize();
+}
+
+// CryptBlocks encrypts or decrypts a number of blocks. The length of
+// src must be a multiple of the block size. Dst and src may point to
+// the same memory.
+func (dec *MyCBCDecrypter) CryptBlocks(dst *[]byte, src []byte) {
+	if len(*dst) < len(src) {
+		panic("len(dst) < len(src)")
+	}
+
+	blockSize := dec.BlockSize()
+
+	if len(src) % blockSize != 0 {
+		panic("src must be a multiple of the block size.")
+	}
+
+	key := src[:blockSize]
+
+	i := 0
+	j := blockSize
+	tempBuff := make([]byte, blockSize)
+
+	for j < len(src) {
+		dec.block.Decrypt(tempBuff, src[j:j+blockSize])
+
+		xorSlice((*dst)[i:i+blockSize], tempBuff, key)
+
+		key = src[j:j+blockSize]
+		i += blockSize
+		j += blockSize
+	}
+
+	//remove padding
+	paddingSize := (int)((*dst)[i-1])
+	*dst = (*dst)[:i-paddingSize]
+}
+
+func NewMyCBCDecrypter(b cipher.Block, iv []byte) *MyCBCDecrypter {
+	if len(iv) != b.BlockSize() {
+		return nil
+	}
+
+	return &MyCBCDecrypter {
+		iv: iv,
+		block: b,
+	}
+}
+
+type MyCTR struct {
+	iv 		[]byte
+	block 	cipher.Block
+}
+
+func (enc *MyCTR) EncryptedSize(srcLen int) int {
+	// source len + iv len + padding len
+	return srcLen + enc.block.BlockSize()
+}
+
+// XORKeyStream XORs each byte in the given slice with a byte from the
+// cipher's key stream. Dst and src may point to the same memory.
+// If len(dst) < len(src), XORKeyStream should panic. It is acceptable
+// to pass a dst bigger than src, and in that case, XORKeyStream will
+// only update dst[:len(src)] and will not touch the rest of dst.
+func (ctr *MyCTR) XORKeyStream(dst *[]byte, src []byte, bEncrypt bool) {
+	additionLen := 0
+	if bEncrypt {
+		additionLen += ctr.block.BlockSize()
+	}
+	if len(*dst) < len(src) + additionLen {
+		panic("len(dst) < len(src) + additionLen")
+	}
+
+	blockSize := ctr.block.BlockSize()
+	tempBuff := make([]byte, blockSize)
+	key := make([]byte, blockSize)
+
+	var j int
+	var i int
+	if bEncrypt {
+		copy(*dst, ctr.iv)
+		copy(key, ctr.iv)
+		j = blockSize
+		i = 0
+	} else {
+		copy(key, src[:blockSize])
+		j = 0
+		i = blockSize
+	}
+
+	last64BitKey := key[blockSize-8:blockSize]
+	var counter uint64 = binary.BigEndian.Uint64(last64BitKey)
+	var blockLen int = blockSize
+
+	for ; i<len(src); {
+		binary.BigEndian.PutUint64(last64BitKey, counter)
+		ctr.block.Encrypt(tempBuff, key)
+
+		if i + blockSize <= len(src) {
+			blockLen = blockSize
+		} else {
+			blockLen = len(src) - i
+		}
+
+		xorSlice((*dst)[j:j+blockLen], src[i:i+blockLen], tempBuff)
+		i += blockLen
+		j += blockLen
+		counter ++
+	}
+
+	*dst = (*dst)[:j]
+}
+
+
+func NewMyCTR(block cipher.Block, iv []byte) *MyCTR {
+	if len(iv) != block.BlockSize() {
+		return nil
+	}
+
+	return &MyCTR{
+		iv: iv,
+		block: block,
+	}
+}
+
+func xorSlice(dst, src, key []byte) []byte {
+	for i:=0; i<len(src); i++ {
+		dst[i] = src[i] ^ key[i]
+	}
+
+	return dst
+}
